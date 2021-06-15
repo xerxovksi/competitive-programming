113b58674f27a4a5ef284448c8acf60a5611162e
Abhishek Das <cse.abhishek.das@gmail.com>
2021-06-15 22:41:55 +0530

    Implemented AddDigits

---
+++ 2021-06-15/AddDigits.cs
@@ -0,0 +1,21 @@
+namespace Google
+{
+    public class Solution
+    {
+        public int AddDigits(int num)
+        {
+            while (num / 10 > 0)
+            {
+                int digitSum = 0;
+                for (int i = num; i > 0; i = i / 10)
+                {
+                    digitSum = digitSum + (i % 10);
+                }
+
+                num = digitSum;
+            }
+
+            return num;
+        }
+    }
+}
